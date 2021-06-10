326e3e21dbf20ed906d54af837b03121372dc1c1
Matthew <44071877+Nastalgua@users.noreply.github.com>
2021-06-10 12:22:08 -0400

    Added practice problems for chapter 4.

---
+++ ch4/templates/template_2.js
@@ -0,0 +1,9 @@
+/** TASK(S)
+ *  1. Create a variable called "sum".
+ *  2. Create a for loop that adds up all the number, including, 1 to 10. 
+ *  3. "sum" should be equal to the number in step (2).
+ *  3. Log to console the variable "sum". 
+ */
+
+// Code here
+
---
+++ ch4/templates/template_3.js
@@ -0,0 +1,7 @@
+/** TASK(S)
+ *  1. Using a while loop, find all numbers divisble by 2 and 3 between 0 to 1000 (inclusive).
+ *  2. Log to console the numbers.
+ */
+
+// Code here
+
